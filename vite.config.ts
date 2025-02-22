e1055b7b2bf61be89eb3262eeeeac68abd3cb861
Rohandede1825 <rohandede97@gmail.com>
2025-02-22 11:43:45 +0530

    feat: update Vite configuration to set output directory to 'dist' and remove manual chunking

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	plugins: [react()],
-	build: {
-	  chunkSizeWarningLimit: 1000, // Increase limit to avoid warnings
-	  rollupOptions: {
-		output: {
-		  manualChunks(id) {
-			if (id.includes("node_modules")) {
-			  return "vendor"; // Create a separate chunk for dependencies
-			}
-		  },
-		},
-	  },
-	},
-  });
-  
+  plugins: [react()],
+  build: {
+    outDir: "dist", 
+  },
+});
